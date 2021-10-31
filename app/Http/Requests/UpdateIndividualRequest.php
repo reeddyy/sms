59424c862a03edae3e5e960f737677f2227af35d
Laravel Daily <info@laraveldaily.com>
2021-10-31 09:29:26 +0000

    QuickAdminPanel automatic commit

--- app/Http/Requests/UpdateIndividualRequest.php
+++ app/Http/Requests/UpdateIndividualRequest.php
@@ -151,6 +151,10 @@ class UpdateIndividualRequest extends FormRequest
                 'string',
                 'nullable',
             ],
+            'special_dietary' => [
+                'string',
+                'nullable',
+            ],
         ];
     }
 }
