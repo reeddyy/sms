e914ae0fd1c5b2ae7280d25cc2b59209142fceab
reeddyy <reddysim@outlook.com>
2021-11-25 17:37:58 +0800

    o-develop-2

--- routes/api.php
+++ routes/api.php
@@ -51,4 +51,10 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', '
 
     // Ada App
     Route::apiResource('ada-apps', 'AdaAppApiController');
+
+    // Qualifications Req
+    Route::apiResource('qualifications-reqs', 'QualificationsReqApiController');
+
+    // Edp Req
+    Route::apiResource('edp-reqs', 'EdpReqApiController');
 });
