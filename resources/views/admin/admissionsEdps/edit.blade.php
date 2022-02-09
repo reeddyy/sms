f95c1144b2ebf0ec94ffb1efb3cc3ae99dcff3f3
unknown <vishalkotak200@gmail.com>
2022-02-09 20:57:28 +0530

    AdmissionsEdp Updated

--- resources/views/admin/admissionsEdps/edit.blade.php
+++ resources/views/admin/admissionsEdps/edit.blade.php
@@ -142,7 +142,7 @@
                                 <label class="required" for="participant_name_id">{{ trans('cruds.admissionsEdp.fields.participant_name') }}</label>
                                 <select class="form-control select2 {{ $errors->has('participant_name') ? 'is-invalid' : '' }}" name="participant_name_id" id="participant_name_id" required>
                                     @foreach($participant_names as $participant_name)
-                                        <option value="{{ $participant_name->id }}" {{ (old('participant_name_id') ? old('participant_name_id') : $enrolmentsQualification->participant_name->id ?? '') == $participant_name->id ? 'selected' : '' }}>{{ $participant_name->name }} | {{ $participant_name->id_no }}</option>
+                                        <option value="{{ $participant_name->id }}" {{ (old('participant_name_id') ? old('participant_name_id') : $admissionsEdp->participant_name->id ?? '') == $participant_name->id ? 'selected' : '' }}>{{ $participant_name->name }} | {{ $participant_name->id_no }}</option>
                                     @endforeach
                                 </select>
                                 @if($errors->has('participant_name'))
