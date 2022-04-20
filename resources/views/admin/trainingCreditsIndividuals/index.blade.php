e5871d1155369b706840be0e1dab16b9e2f39015
reeddyy <73637911+reeddyy@users.noreply.github.com>
2022-04-20 13:30:45 +0800

    Update index.blade.php
    Added 1 filter - <input class="search" type="text" placeholder="{{ trans('global.search') }}">

--- resources/views/admin/trainingCreditsIndividuals/index.blade.php
+++ resources/views/admin/trainingCreditsIndividuals/index.blade.php
@@ -65,6 +65,7 @@
                             <input class="search" type="text" placeholder="{{ trans('global.search') }}">
                         </td>
                         <td>
+                            <input class="search" type="text" placeholder="{{ trans('global.search') }}">
                         </td>
                         <td>
                             <input class="search" type="text" placeholder="{{ trans('global.search') }}">
